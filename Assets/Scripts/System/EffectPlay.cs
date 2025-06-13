ed97730aae1737e643e733ce05b175a5454ab560
Nerlve <yuma9359@gmail.com>
2025-06-13 15:25:04 +0900

    EffectSystem改良

--- Assets/Scripts/System/EffectPlay.cs
+++ Assets/Scripts/System/EffectPlay.cs
@@ -1,58 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
+using MySystem;
 using UnityEngine;
 
-
-[System.Serializable]
-public class pair<TKey, TValue>
-{
-    public TKey Key;
-    public TValue Value;
-
-
-    public pair(TKey first, TValue second)
-    {
-        this.Key = first;
-        this.Value = second;
-    }
-
-}
 
 public class EffectPlay : MonoBehaviour
 {
     const int EFFECT_DATA = 5;
 
-    // -----�ϐ��錾
-    public Vector3 _playPos = new Vector3(99999, 99999, 99999);
+    
+    public Vector3 playPos = new Vector3(99999, 99999, 99999);
 
-    // �G�t�F�N�g�f�[�^���C���X�y�N�^�[�Őݒ�
     [SerializeField]
-    pair<string, GameObject>[] effectData;
-
-    Dictionary<string, GameObject> effect = new Dictionary<string, GameObject>(EFFECT_DATA);
+    MyDictionary<string, GameObject> effects;
 
     float lifeTime = 1.3f;
-
-    private pair<string, GameObject> currentEffect = new pair<string, GameObject>("none",null); // ���ݐ�������Ă���W���X�g����G�t�F�N�g�̎Q�Ƃ�ێ�����ϐ�
-
 
     // Start is called before the first frame update
     void Start()
     {
-       // �ݒ肳�ꂽ�f�[�^��map�Ɉړ�
-       for(int i = 0; i < effectData.Length; i++)
-       {
-            if (effectData[i].Value == null)
-                continue;
+        effects.Initialize();
 
-            effect.Add(effectData[i].Key, effectData[i].Value);
-       }
-
-        // �G�f�B�^����f�[�^���ݒ肳��Ă��Ȃ������ꍇ,
-        // �A�^�b�`����Ă���I�u�W�F�N�g�̃|�W�V�������擾
-        if (_playPos == new Vector3(99999, 99999, 99999))
+        if (playPos == new Vector3(99999, 99999, 99999))
         {
-            _playPos = transform.position;
+            playPos = transform.position;
         }
     }
 
@@ -62,33 +32,21 @@ public class EffectPlay : MonoBehaviour
 
     }
 
-    /// <summary>
-    /// �G�t�F�N�g�𐶐�
-    /// </summary>
-    /// <param name="effectname">�g�p�������G�t�F�N�g�̖��O</param>
-    /// <param name="addPos">���Z�������l default = 0,0,0</param>
-    public void InstantiateEffect(in string effectname, in Vector3? addPos = null)
+    public void Play(in string effectname, in Vector3? addPos = null)
     {
-        _playPos = transform.position;
-        // ���W�ɃG�t�F�N�g����
-        _playPos += addPos ?? Vector3.zero;
+        playPos = transform.position;
+        
+        playPos += addPos ?? Vector3.zero;
 
-        if (currentEffect.Key != effectname)
+        if (effects.Dict[effectname] != null)
         {
-            currentEffect.Key = effectname;
-            currentEffect.Value = Instantiate(effect[effectname], _playPos, Quaternion.identity);
+            GameObject currentEffect = Instantiate(effects.Dict[effectname], playPos, Quaternion.identity);
 
-            StartCoroutine(DestroyObject(currentEffect.Value, lifeTime));
+            StartCoroutine(DestroyObject(currentEffect, lifeTime));
         }
-
-    
     }
 
-    /// <summary>
-    /// �I�u�W�F�N�g���w�肳�ꂽ���Ԃō폜
-    /// </summary>
-    /// <param name="obj">�폜�������I�u�W�F�N�g</param>
-    /// <param name="deley">��������</param>
+   
     private IEnumerator DestroyObject(GameObject obj, float deley)
     {
         yield return new WaitForSeconds(deley);
