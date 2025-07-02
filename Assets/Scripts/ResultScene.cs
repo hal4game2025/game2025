ab5eb3bfc6cc403e209112fdfb388c1f7cd51e1e
TomoSANTA <t24n19@icloud.com>
2025-07-02 17:31:57 +0900

    リザルト画面で残りの体力表示

--- Assets/Scripts/ResultScene.cs
+++ Assets/Scripts/ResultScene.cs
@@ -1,34 +1,57 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class ResultScene : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI resultTimeText; // リザルトタイム表示用
+   [SerializeField] TextMeshProUGUI resultTimeText; // リザルトタイム表示用
+                                                    // [SerializeField] RawImage[] HpUI = new RawImage[20];//UIの数
+                                                    // [SerializeField] Texture2D[] HpTex = new Texture2D[2];//表示する画像
+    Texture2D[] HpTex;
+    RawImage[] HpUI;
     TimeSystem timeSystem;
     SceneManager sceneManager;
     PlayerControls controls;
+    PlayerHpUI playerHpUI;
 
     void Start()
     {
         sceneManager = SceneManager.Instance;
         timeSystem = TimeSystem.Instance;
+        playerHpUI = PlayerHpUI.Instance;
+
         controls = new PlayerControls();
 
         controls.UI.Confirm.performed += Confirm;
         controls.Enable();
 
+        //HPを表示するためのメソッド呼び出し
+        int HP = playerHpUI.GetHp(); // HPを取得して表示するためのメソッド呼び出し
+        HpTex = playerHpUI.GetHPTex(); // HPのUIデータを取得する
+        HpUI = playerHpUI.GetHpUI(); // HPのUI数を取得する
 
-        //ここでリザルトタイムを表示
+
+
+
+        //ここでリザルトタイムとHPを表示
         if (timeSystem != null && resultTimeText != null)
         {
             float time = timeSystem.GetTime();
             Debug.Log("ResultScene: Time = " + time);
             System.TimeSpan span = new System.TimeSpan(0, 0, (int)time);
             resultTimeText.text = span.ToString(@"mm\:ss");
-
             timeSystem.ResetTime(); //リザルト表示後に時間をリセット
+
+
+            for (int i = playerHpUI.GetPlayerHpCount(); i < 20; i++)
+            {
+                HpUI[i].texture = HpTex[0];
+
+            }
         }
+        
     }
 
     void Confirm(InputAction.CallbackContext context)
--- Assets/Scripts/UI/PlayerHpUI/PlayerHpUI.cs
+++ Assets/Scripts/UI/PlayerHpUI/PlayerHpUI.cs
@@ -1,31 +1,75 @@
 using UnityEngine;
 using UnityEngine.UI;
-public class PlayerHpUI : MonoBehaviour
+public class PlayerHpUI : SingletonMonoBehaviour<PlayerHpUI>
 {
-    [SerializeField] Texture2D[] HpTex=new Texture2D[2];
+    [SerializeField] Texture2D[] HpTex = new Texture2D[2];
     [SerializeField] RawImage[] HpUI = new RawImage[20];
     private RawImage[] playerHp;
     [SerializeField] PlayerStatus playerStatus;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private  int maxHp;
+    public static int playerHpCount;// プレイヤーのHPの数
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(this.gameObject);
+    }
     void Start()
     {
-        int maxHp;
-        maxHp = playerStatus.HP;
-        playerHp = new RawImage[maxHp];
-        for (int i = maxHp; i < 20; i++)
+        if (playerStatus != null)
         {
-            HpUI[i].enabled = false;
+            maxHp = playerStatus.HP;
+            playerHp = new RawImage[maxHp];
+            for (int i = maxHp; i < 20; i++)
+            {
+                HpUI[i].enabled = false;
+            }
+
         }
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        for (int i = playerStatus.HP; i < 20; i++)
+        if(playerStatus != null)
         {
-            HpUI[i].texture = HpTex[0];
+            //HPの値をplayerHpCountに同期
+            playerHpCount = playerStatus.HP;
 
+            for (int i = playerStatus.HP; i < 20; i++)
+            {
+                HpUI[i].texture = HpTex[0];
+            }
         }
+
+    }
+
+    /// <summary>
+    /// HPのUIを取得する
+    /// </summary>
+    /// <returns></returns>
+    public Texture2D[] GetHPTex()
+    {
+        return HpTex;
+    }
+    public RawImage[] GetHpUI()
+    {
+        return HpUI;
+    }
+
+    /// <summary>
+    /// HPを取得する
+    /// </summary>
+    /// <returns></returns>
+    public int GetHp()
+    {
+        return maxHp;
+    }
+    /// <summary>
+    /// プレイヤーのHPの数を取得する
+    /// </summary>
+    /// <returns></returns>
+    public int GetPlayerHpCount()
+    {
+        return playerHpCount;
     }
 }
