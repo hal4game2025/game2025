7a28f2d99ccdce48c93458295d73bc0f78224319
choco256 <ryu612324@gmail.com>
2025-07-16 14:15:19 +0900

    ゆうまに渡す用です

--- Assets/Scripts/UI/EnemyHpUI/EnemyHp.cs
+++ Assets/Scripts/UI/EnemyHpUI/EnemyHp.cs
@@ -1,23 +1,21 @@
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class EnemyHp : MonoBehaviour
 {
     [SerializeField]private EnemyStatus enemyStatus;
-    UIDocument enemyHpUIDocument;
-    UnityEngine.UIElements.ProgressBar progressBar;
+    [SerializeField] private Image hpBar;
+    [SerializeField] private Image damageBar;
+
     void Start()
     {
-        
-        enemyHpUIDocument = GetComponent<UIDocument>();
-        progressBar = enemyHpUIDocument.rootVisualElement.Q<ProgressBar>("EnemyHp");
+
         
     }
 
     void Update()
     {
-        progressBar.value = enemyStatus.HP;
 
     }
 }
